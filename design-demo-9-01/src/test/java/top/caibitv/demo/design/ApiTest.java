a0e535c37ae82879cfccfa81f301a1d7081b8026
CAIBITV <yyt14588@gmail.com>
2024-06-07 10:32:03 +0800

    feat: 9-装饰器模式-反例

---
+++ design-demo-9-01/src/test/java/top/caibitv/demo/design/ApiTest.java
@@ -0,0 +1,16 @@
+package top.caibitv.demo.design;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ApiTest {
+
+    @Test
+    public void test_LoginSsoDecorator() {
+        LoginSsoDecorator ssoDecorator = new LoginSsoDecorator();
+        String request = "1successhuahua";
+        boolean success = ssoDecorator.preHandle(request, "ewcdqwt40liuiu", "t");
+        System.out.println("登录校验：" + request + (success ? " 放行" : " 拦截"));
+    }
+}
