afd08fb3f739eb58dfa618a8674a4c456bcbcc3b
CAIBITV <yyt14588@gmail.com>
2024-06-14 09:14:27 +0800

    feat: 16-中介者模式-反例-使用JDBC方式连接数据库

---
+++ design-demo-16-01/src/main/java/top/caibitv/demo/design/JDBCUtil.java
@@ -0,0 +1,32 @@
+package top.caibitv.demo.design;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
+
+public class JDBCUtil {
+
+    private static Logger logger = LoggerFactory.getLogger(JDBCUtil.class);
+
+    private static final String URL = "jdbc:mysql://127.0.0.1:23306/ruoyi_pet";
+
+    private static final String USER = "root";
+
+    private static final String PASSWORD = "123456";
+
+    public static void main(String[] args) throws ClassNotFoundException, SQLException {
+        // 1.加载驱动程序
+        Class.forName("com.mysql.jdbc.Driver");
+        // 2. 获得数据库连接
+        Connection connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        // 3.操作数据库，实现增删改查
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM sys_role");
+        // 如果有数据，rs.next()返回true
+        while (resultSet.next()) {
+            logger.info("测试结果：{}", resultSet.getString("role_name"));
+        }
+    }
+}
