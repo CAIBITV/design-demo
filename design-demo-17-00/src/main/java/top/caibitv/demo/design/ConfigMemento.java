371f25c9c6ed14132a044b61dfc1e99f54eea6e6
CAIBITV <yyt14588@gmail.com>
2024-08-09 17:12:34 +0800

    feat: 17-备忘录模式

---
+++ design-demo-17-00/src/main/java/top/caibitv/demo/design/ConfigMemento.java
@@ -0,0 +1,20 @@
+package top.caibitv.demo.design;
+
+/**
+ * 配置文件备忘录
+ */
+public class ConfigMemento {
+    private ConfigFile configFile;
+
+    public ConfigMemento(ConfigFile configFile) {
+        this.configFile = configFile;
+    }
+
+    public ConfigFile getConfigFile() {
+        return configFile;
+    }
+
+    public void setConfigFile(ConfigFile configFile) {
+        this.configFile = configFile;
+    }
+}
