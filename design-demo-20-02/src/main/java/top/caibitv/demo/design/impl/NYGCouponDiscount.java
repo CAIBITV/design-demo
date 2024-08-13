8d045881c50ae254f4f4bf05c5e56739516c1e83
CAIBITV <yyt14588@gmail.com>
2024-08-13 16:19:19 +0800

    feat: 20-策略模式-重构

---
+++ design-demo-20-02/src/main/java/top/caibitv/demo/design/impl/NYGCouponDiscount.java
@@ -0,0 +1,20 @@
+package top.caibitv.demo.design.impl;
+
+import top.caibitv.demo.design.ICouponDiscount;
+
+import java.math.BigDecimal;
+
+/**
+ * n元购买
+ */
+public class NYGCouponDiscount implements ICouponDiscount<Double> {
+
+    /**
+     * n元购购买
+     * 1. 无论原价多少钱都固定金额购买
+     */
+    public BigDecimal discountAmount(Double couponInfo, BigDecimal skuPrice) {
+        return new BigDecimal(couponInfo);
+    }
+
+}
